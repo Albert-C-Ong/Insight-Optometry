1d93af82416cf193d3e6259a792d064d66941054
Albert Ong <albert_ong_us@yahoo.com>
2022-07-26 17:38:10 -0700

    Initialized mobile.css

--- pages/contact.php
+++ pages/contact.php
@@ -39,6 +39,7 @@ if (isset($_POST["submit"])) {
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <link rel="stylesheet" type="text/css" href="../assets/main.css">
+<link rel="stylesheet" type="text/css" href="../assets/mobile.css">
 </head>    
 
 <body>
@@ -74,7 +75,7 @@ if (isset($_POST["submit"])) {
 <div class="container">
   <div class="row">
     <div class="col-6">
-      <h1 style="font-size: 55px;">WE'D LOVE TO<br>HEAR FROM YOU!</h1>
+      <h1 class="h1-contact">WE'D LOVE TO<br>HEAR FROM YOU!</h1>
       
       <form method="POST">
         <div class="container" style="margin-top: 20px;">
